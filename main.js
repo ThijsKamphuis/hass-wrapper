da9fe752f3feac66fa2faed48c095b71cdc94e6b
Thijs Kamphuis <thijskamphuis@icloud.com>
2024-07-08 02:25:59 +0200

    init

---
+++ main.js
@@ -0,0 +1,21 @@
+const { app, BrowserWindow } = require('electron')
+
+const createWindow = () => {
+    const win = new BrowserWindow({
+      width: 596,
+      height: 1390,
+      frame: false,
+      icon: "hass.png",
+      show: true,
+    })
+  
+    win.loadURL('https://hass.thijsk.systems')
+    win.removeMenu()
+    win.setMaximumSize(596, 9999)
+    win.setPosition(1964, 0)
+    win.setAlwaysOnTop(false, 'normal')
+  }
+
+  app.whenReady().then(() => {
+    createWindow()
+  })
